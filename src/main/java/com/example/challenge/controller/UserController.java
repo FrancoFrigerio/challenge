30f978c99cc22f6e43e3eb2e6a98fa606abe646c
FRANCO <franco.p.f@live.com.ar>
2021-12-05 18:47:15 -0300

    add UserController

---
+++ src/main/java/com/example/challenge/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.challenge.controller;
+
+
+import com.example.challenge.service.CommentService;
+import com.example.challenge.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/")
+    public ResponseEntity<?> getUsers(){
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?>getUserPhotos(@PathVariable(name = "id") Long id){
+        return ResponseEntity.ok().body(userService.getPhotos(id));
+    }
+
+}
